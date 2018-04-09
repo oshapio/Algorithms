3d05fcb83264129c34b6f463f5e9028d24f06b93
oshapio <643050@gmail.com>
2018-04-09 11:07:12 +0300

    Fixed pollard's rho algo

--- Math/Pollards_rho_logarithms.cpp
+++ Math/Pollards_rho_logarithms.cpp
@@ -14,7 +14,7 @@ typedef long long ll;
 typedef tuple<int, int, int> t3;
 typedef vector<int> vi;
 // Finds x in  g ^ x = alpha MOD(N) 
-// Only works(?) when g is a generator and N is a prime number
+// Only works when order of group doesn't have small factors
 int modExp(int x, int pow, int mod) {
 	int res = 1;
 	while (pow) {
@@ -33,13 +33,11 @@ vi extendEuclid(int a, int b) {
 
 int findInverse(int x, int mod) {
 	vi interRes = extendEuclid(x, mod);
-	if (interRes[0] != 1) {
+	if (interRes[0] != 1)
+		// Inverse doesn't exist!
 		return -1;
-		// gcd is not 1
-		//cout << "GCD is not 1, exiting..";
-		//throw invalid_argument("X and mod are not coprime!");
-	}
-	return (extendEuclid(x, mod)[1] + mod) % mod;
+	
+	return (interRes[1] + mod) % mod;
 }
 
 vi getNext(vi args,int g, int alpha, int order, int N) {
@@ -61,9 +59,15 @@ vi getNext(vi args,int g, int alpha, int order, int N) {
 	}
 	return { x,a,b };
 }
-
-int pollardsRhoDL(int g, int alpha, int order, int N, int x = 1, int a = 0, int b = 0) {
+int getRand(int from, int to) {
+	int range = to - from + 1;
+	int numb = rand() % (range);
+	return from + numb;
+}
+int pollardsRhoDL(int g, int alpha, int order, int N, int a = 0, int b = 0, int tries = 0) {
+	if (tries >= 10) return -1;
 	// init vals
+	int x = (modExp(g, a, N) * modExp(alpha, b, N)) % N;
 	vi slow = { x, a, b}, fast = { x, a, b};
 	for (int i = 0; i < N + 1; i++) {
 		slow = getNext(slow, g, alpha, order, N);
@@ -73,34 +77,38 @@ int pollardsRhoDL(int g, int alpha, int order, int N, int x = 1, int a = 0, int 
 		if (slow[0] == fast[0]) {
 			int rem = (slow[2] - fast[2]);
 			if (rem == 0) {
-				cout << "Can't find solution\n";
-				return -1;
+				//cout << "Can't find solution\n";
+				break;
 			}
 			else {
 				// Answer found
 				int inv = findInverse(rem, order);
-				
+				if (inv == -1) {
+					// No inverse exists, reinitialize values
+					break;
+				}
 				return ((findInverse(rem, order) * (fast[1] - slow[1]))) % (order);
 			}
 		}
 	}
-	return -1;
+	// Try again with different values
+	return pollardsRhoDL(g, alpha, order, N, getRand(1, order - 1), getRand(1, order - 1), tries + 1);
 }
 
 int main()
 {
-	 vector<int> tests[]= {
-		// g, N, order, alpha, answer
-		{7, 499, 498, 12, 321},
-		{3, 7, 6, 2, 2},
-		{2, 383, 191, 228, 110},
-		{2, 1019, 1018, 5, 10}
-     };
-	 for (auto test : tests) {
-		 int g = test[0], N = test[1], order = test[2], alpha = test[3], answer = test[4];
-		 int temp = pollardsRhoDL(g, alpha, order, N);
-		 cout << temp << " " << ((temp) % order + order) % order << " " << answer << "\n";
-	 }
+	vector<int> tests[]= {
+	// g, N, order, alpha, answer
+	{7, 499, 498, 12, 321},
+	{3, 7, 6, 2, 2},
+	{2, 383, 191, 228, 110},
+	{2, 1019, 1018, 5, 10}
+    };
+	for (auto test : tests) {
+		int g = test[0], N = test[1], order = test[2], alpha = test[3], answer = test[4];
+		int temp = pollardsRhoDL(g, alpha, order, N);
+		cout << temp << " " << ((temp) % order + order) % order << " " << answer << "\n";
+	}
 	return 0;
 }
 
